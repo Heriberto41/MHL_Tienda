42219c6840379afdec7f84e1f2315913e9ddd7aa
can16031997. <1715110167@utec-tgo.edu.mx>
2016-10-20 13:45:01 -0500

    Actualización

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -17,8 +17,8 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-      View_Proveedor proveedor = new View_Proveedor();
-      Controller_Proveedores controller_Proveedores = new Controller_Proveedores(proveedor);
+      View_Producto proveedor = new View_Producto();
+      Controller_Producto controller_Proveedores = new Controller_Producto(proveedor);
     }
     
 }
